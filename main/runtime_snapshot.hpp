fa4ab7a12a6c3c64bf5ed7fc8989e5fd2c157ee9
Cheng <chiacheng.tsao@gmail.com>
2026-09-21 01:31:33 +0800

    Revert unused telemetry snapshot field

--- main/runtime_snapshot.hpp
+++ main/runtime_snapshot.hpp
@@ -7,8 +7,6 @@ namespace triwhirl::runtime {
 struct RuntimeSnapshot {
   std::uint32_t t_us = 0U;
 
-  bool telemetry_enabled = false;
-
   bool attitude_initialized = false;
   bool attitude_valid = false;
   float attitude_angle_rad = 0.0F;
