58a8540350ab111dcf139ea60165f865dae0f2fe
Cheng <chiacheng.tsao@gmail.com>
2026-09-21 11:43:07 +0800

    Add realtime balance mode interface

---
+++ main/runtime_balance.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "triwhirl/balance_controller.hpp"
+
+namespace triwhirl::runtime {
+
+enum class BalanceStartFailure : int {
+  kNone = 0,
+  kAlreadyActive,
+  kMotorActive,
+  kControllerConfig,
+  kMotorConfig,
+  kEncoder,
+  kImu,
+  kAttitude,
+  kSafetyFault,
+  kOutsideCapture,
+  kWheelRate,
+};
+
+struct RuntimeBalanceStatus {
+  bool configured = false;
+  bool active = false;
+  triwhirl::BalanceControllerConfig config{};
+  triwhirl::BalanceControllerOutput output{};
+};
+
+bool configureRuntimeBalance(const triwhirl::BalanceControllerConfig& config);
+BalanceStartFailure startRuntimeBalance(float* initial_vq_v = nullptr);
+void stopRuntimeBalance();
+void updateRuntimeBalance();
+bool runtimeBalanceActive();
+RuntimeBalanceStatus runtimeBalanceStatus();
+const char* balanceStartFailureName(BalanceStartFailure failure);
+
+}  // namespace triwhirl::runtime
